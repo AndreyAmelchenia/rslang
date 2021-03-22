6cac3fd6b8e02965942baea3bb03fcc82cbb06fc
Evgeniy Vasilko <56649443+ShadeZP@users.noreply.github.com>
2021-03-22 11:19:22 +0200

    fix: fixed commets from revue

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,10 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AngularMaterialModule} from '../material'
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularMaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    AngularMaterialModule
-  ],
-  exports: [
-    AngularMaterialModule
-  ]
+  imports: [CommonModule, AngularMaterialModule],
+  exports: [AngularMaterialModule],
 })
-export class SharedModule {
-}
+export class SharedModule {}
