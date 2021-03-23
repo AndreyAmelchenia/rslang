62d77ae19a18597a0f97e422b98e8890e1583799
DobrovskayaElena <lenayalena@tut.by>
2021-03-23 18:47:19 +0300

    feat: create about us component #8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
-
+import { AboutUsModule } from "./aboutUs/module/about-us/about-us.module";
+import { AboutUsService } from "./aboutUs/service/about-us.service";
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, BrowserAnimationsModule, AboutUsModule],
+  providers: [AboutUsService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
