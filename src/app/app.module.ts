63c86016f9e8f1f5fb43efdb5de165bbd581cc04
Nikolay Nikityuk <55094494+micolka@users.noreply.github.com>
2021-03-23 21:27:44 +0300

    feat: create empty navar and footer components (#29)
    * feat: create empty navar and footer components
    
    * feat: move nav, footer components to navigaion module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
+import { NavigationModule } from './navigation/navigation.module';
 
-import { AboutUsModule } from "./aboutUs/module/about-us/about-us.module";
-import { AboutUsService } from "./aboutUs/service/about-us.service";
+import { AboutUsModule } from './aboutUs/module/about-us/about-us.module';
+import { AboutUsService } from './aboutUs/service/about-us.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AboutUsModule, SharedModule],
+  imports: [BrowserModule, BrowserAnimationsModule, SharedModule, NavigationModule, AboutUsModule],
   providers: [AboutUsService],
   bootstrap: [AppComponent],
 })
--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { LayoutModule } from '@angular/cdk/layout';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NavBarComponent, FooterComponent],
+  exports: [NavBarComponent],
+  imports: [CommonModule, LayoutModule, SharedModule],
 })
-export class NavigationModule { }
+export class NavigationModule {}
