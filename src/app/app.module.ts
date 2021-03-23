6275b010b00aa08bd242cb8e1696b45c124ed624
micolka <mic_la@tut.by>
2021-03-23 19:52:38 +0300

    feat: move nav, footer components to navigaion module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, FooterComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, SharedModule, NavigationModule],
   providers: [],
   bootstrap: [AppComponent],
 })
