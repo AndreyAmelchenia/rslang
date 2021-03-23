1106f2c8bc90f48b8e54e06a0fa26a1040e422b7
AndreyAmelchenia <pmad1986@gmail.com>
2021-03-23 13:39:04 +0300

    feat: add action reduser and get for word

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { StateModule } from './state/state.module';
 import { SharedModule } from './shared/shared.module';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +17,11 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     StoreModule.forRoot({}),
     StateModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/state/state.module.ts
+++ src/app/state/state.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { wordsFeatureKey, booksReducer } from './reducers/words.reducer';
 
 @NgModule({
   declarations: [],
-  imports: [StoreModule.forFeature()],
+  imports: [StoreModule.forFeature(wordsFeatureKey, booksReducer)],
 })
 export class StateModule {}
