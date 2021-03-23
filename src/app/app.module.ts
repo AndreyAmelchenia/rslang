cdaa83598d79898df2d9ca10724ec1afceb8b653
AndreyAmelchenia <pmad1986@gmail.com>
2021-03-23 11:14:19 +0300

    feat: add state module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
+import { StateModule } from './state/state.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule.forRoot({})],
+  imports: [BrowserModule, StoreModule.forRoot({}), StateModule],
   providers: [],
   bootstrap: [AppComponent],
 })
