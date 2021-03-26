dfbe3645bc42a6918cc3a1becd4ad28d042d8ed4
803142 <803142@gmail.com>
2021-03-26 17:07:13 +0300

    refaktor

--- src/app/games/components/audio-challenge-game/state/audio-challenge-game.state.ts
+++
@@ -1,8 +0,0 @@
-export interface State {
-  isGameStarted: boolean;
-  wordsRemaining: number;
-  color: string;
-}
-
-// eslint-disable-next-line prettier/prettier
-export const initialState: State = {} as State;
