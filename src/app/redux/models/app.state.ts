b9d1a9494156f94cc9f6d635422c5b9951ed1d97
Evgeniy Vasilko <56649443+ShadeZP@users.noreply.github.com>
2021-04-07 08:43:48 +0300

    fix: fix duplicated satate models

--- src/app/redux/models/app.state.ts
+++
@@ -1,7 +0,0 @@
-import { Word } from '../../common/models/word.model';
-import { ILoginState } from './loginState.models';
-
-export interface AppState {
-  words: ReadonlyArray<Word>;
-  login: ILoginState;
-}
