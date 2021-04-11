b377970bebe7680f3c26a3746125c248630c3208
Andey Amelchenia <pmad1986@gmail.com>
2021-04-12 00:37:51 +0300

    fix: correct geme list

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { SessionService } from 'src/app/common/services/storage/session.service';
-import { LoadListGame } from 'src/app/redux/actions/listGame.actions';
 import { AppState } from 'src/app/redux/app.state';
 
 @Component({
@@ -9,14 +7,8 @@ import { AppState } from 'src/app/redux/app.state';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  constructor(private store: Store<AppState>, private userSession: SessionService) {}
+export class AppComponent {
+  constructor(private store: Store<AppState>) {}
 
   title = 'rslang';
-
-  ngOnInit() {
-    if (this.userSession.getItem('user')) {
-      this.store.dispatch(LoadListGame({ group: 3, page: 0, wordsPerPage: 20 }));
-    }
-  }
 }
