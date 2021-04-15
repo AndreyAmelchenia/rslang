5ba41e589ee907d331624cf2b210ff05aa8e942b
DobrovskayaElena <lenayalena@tut.by>
2021-04-15 17:42:01 +0300

    fix: create settings dialog

--- src/app/components/games/components/games-settings/games-settings.component.ts
+++ src/app/components/games/components/games-settings/games-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { GamesSettingsDialogComponent } from '../games-settings-dialog/games-settings-dialog.component';
 
 @Component({
   selector: 'app-games-settings',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./games-settings.component.scss'],
 })
 export class GamesSettingsComponent {
+  wordsAmount: number[] = [10, 20, 30, 40, 50, 60];
+
+  level: number[] = [1, 2, 3, 4, 5, 6];
+
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
+
+  openDialogSettings() {
+    const dialogRef = this.dialog.open(GamesSettingsDialogComponent, {
+      width: '400px',
+      height: '400px',
+      data: {
+        wordsAmountData: this.wordsAmount,
+        levelChosen: this.level,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe();
+  }
 }
